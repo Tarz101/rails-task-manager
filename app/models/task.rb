a60eaff5b52bf55fda48232a439c531a186181bf
Othmane Laghzaouni <o.laghzaouni@gmail.com>
2020-02-11 16:53:10 +0100

    exo2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,2 @@
 class Task < ApplicationRecord
-  def create
-    create_table :tasks do |t|
-      t.string :title
-      t.text :details
-      t.boolean :completed
-
-      t.timestamps
-    end
-  end
 end
